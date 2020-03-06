0e9cb6a4351e707e99b71de73e031b7babfbfa05
andriamasy-jonatana <jonatana-aro-jesy.andriamasy@etu.univ-amu.fr>
2020-03-06 17:44:50 +0100

    edit login page ajout des fonctionnalites

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  id = '';
+  mdp = '';
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.loginService.login(this.id, this.mdp);
   }
 
 }
