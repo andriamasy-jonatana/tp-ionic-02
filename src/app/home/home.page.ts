34a586ebc11fdc99cb75e12123ea70fb4d28b03a
andriamasy-jonatana <jonatana-aro-jesy.andriamasy@etu.univ-amu.fr>
2020-03-06 16:26:29 +0100

    ajout login page et edit routing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {LoginService} from '../services/login.service';
+import User from '../services/models/User';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +10,18 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   title: string;
+  private user: User;
 
-  constructor() {}
+  constructor(private loginService: LoginService) {}
 
   updateTitle() {
     this.title = 'Un nouveau titre';
+    this.user = this.loginService.login('identification', 'P@ssw0rd');
+    console.log(this.user);
+  }
+
+  maNouvelleMethodeQuiNeSeraPasTeste() {
+    this.title = 'Mon nouveau titre';
   }
 
 }
